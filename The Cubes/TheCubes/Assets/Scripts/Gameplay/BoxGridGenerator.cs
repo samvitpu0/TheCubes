84ac67ff6e2a9420844bd023e87167e74facb635
Samvit Purohit <samvitpu0@gmail.com>
2019-05-05 04:11:48 +0530

    Added Box Grid Genarator, Box Prefab, Material for box
    * Added the script which genarates the box grid for now its makes a grid of order 5.
    * Added a prefab for box
    * Added a material if multicolor boxes are needed

--- The Cubes/TheCubes/Assets/Scripts/Gameplay/BoxGridGenerator.cs
+++ The Cubes/TheCubes/Assets/Scripts/Gameplay/BoxGridGenerator.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class BoxGridGenerator : MonoBehaviour
 {
+    /// <summary>
+    /// Parent under which all the boxes will be created 
+    /// </summary>
+    public Transform gridParent;
+
+    /// <summary>
+    /// Instance of one box, change in this will reflect to all the boxes in the grid
+    /// </summary>
+    public GameObject boxInstance;
+
+    /// <summary>
+    /// Defines the number of boxes on each axism, for testing purpose its set to default value of 5
+    /// </summary>
+    private int _gridOrder = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenarateGrid();
+    }
+    
+    /// <summary>
+    /// Generates Grid of Boxes according to _gridOrder 
+    /// </summary>
+    private void GenarateGrid()
+    {
+        for(int i = 0; i < _gridOrder; i++)
+        {
+            for(int j = 0; j < _gridOrder; j++)
+            {
+                for(int k = 0; k < _gridOrder; k++)
+                {
+                    var _box = Instantiate(boxInstance, new Vector3(i,j,k),Quaternion.identity, gridParent) as GameObject;
+                }
+            }
+        }
+
+        gridParent.transform.position = new Vector3(-2f, -2f, -2f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
