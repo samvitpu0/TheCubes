729ea752f8837ef6108deb1af62a82e523b16fa4
Samvit Purohit <samvitpu0@gmail.com>
2019-05-05 04:44:07 +0530

    Mouse/Finger Trail Added

---
+++ The Cubes/TheCubes/Assets/Scripts/Gameplay/TrailScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailScript : MonoBehaviour
+{
+    public float speed = 8.0f;
+    public float distanceFromCamera = 5.0f;
+
+    void Update()
+    {
+        Vector3 _mousePosition = Input.mousePosition;
+        _mousePosition.z = distanceFromCamera;
+
+        Vector3 _mouseScreenToWorld = Camera.main.ScreenToWorldPoint(_mousePosition);
+
+        Vector3 _position = Vector3.Lerp(transform.position, _mouseScreenToWorld, 1.0f - Mathf.Exp(-speed * Time.deltaTime));
+
+        transform.position = _position;
+    }
+}
