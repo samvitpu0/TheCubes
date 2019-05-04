3e46d8ad0e6b5c6a630c54efbe2f55806e62870f
Samvit Purohit <samvitpu0@gmail.com>
2019-05-05 02:02:22 +0530

    Created Scriptable Objects to Create  Box Color Data

---
+++ The Cubes/TheCubes/Assets/Scripts/ScriptableObjects/GridBlockColors.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used to create scriptable object for box color in which user can choos 5 options of color from which
+/// random color will be picked to color the grid
+/// </summary>
+[CreateAssetMenu(fileName = "BoxColorData", menuName = "BoxGrid/ColorData", order = 1)]
+public class GridBlockColors : ScriptableObject
+{
+    public Color32 colorOption_One;
+    public Color32 colorOption_Two;
+    public Color32 colorOption_Three;
+    public Color32 colorOption_Four;
+    public Color32 colorOption_Five;
+}
