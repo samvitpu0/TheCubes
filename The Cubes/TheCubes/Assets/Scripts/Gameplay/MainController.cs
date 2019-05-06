6242f050d454477194b8a66c9d736e8d7785dbc5
Samvit Purohit <samvitpu0@gmail.com>
2019-05-07 02:33:33 +0530

    Trail Fix
    * Trail will now only created while finger is held down
    * Project  name, Company name, Optimal Memory usage set

---
+++ The Cubes/TheCubes/Assets/Scripts/Gameplay/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public GameObject fingerTrail;
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            fingerTrail.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            fingerTrail.SetActive(true);
+        }
+        else
+        {
+            fingerTrail.SetActive(false);
+        }
+    }
+}
