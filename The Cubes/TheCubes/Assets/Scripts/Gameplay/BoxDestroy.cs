6dc32795733777303bcbaa660045a99e835ad60d
Samvit Purohit <samvitpu0@gmail.com>
2019-05-07 05:47:06 +0530

    Two finger rotation and some fixes
    * Grid will rotate when swipe action is performed with 2 fingers.
    * Disabled box destroy input while rotating.
    * Disables trail while rotating.

--- The Cubes/TheCubes/Assets/Scripts/Gameplay/BoxDestroy.cs
+++ The Cubes/TheCubes/Assets/Scripts/Gameplay/BoxDestroy.cs
@@ -6,6 +6,8 @@ public class BoxDestroy : MonoBehaviour
 {
     private void OnMouseEnter()
     {
+        // If two or more fingers are touching the screen then Box will not be destroyed
+        if(MainController.canDestroyBlocks)
         Destroy(gameObject);
     }
 }
--- The Cubes/TheCubes/Assets/Scripts/Gameplay/MainController.cs
+++ The Cubes/TheCubes/Assets/Scripts/Gameplay/MainController.cs
@@ -4,18 +4,65 @@ using UnityEngine;
 
 public class MainController : MonoBehaviour
 {
+    /// <summary>
+    /// The value will be changed to false when 2 fingers are touching the screen, if false the Grid Boxes
+    /// cannot be destroyed. Set back to true when only one finger is touching the screen.
+    /// </summary>
+    [HideInInspector]
+    public static bool canDestroyBlocks = true;
+
+    /// <summary>
+    /// Gameobject of the Trail Particle System
+    /// </summary>
+    [Header("Gameobject of the Trail Particle System")]
     public GameObject fingerTrail;
+
+    /// <summary>
+    /// Root Gameobject of the Grid
+    /// </summary>
+    [Header("Root Gameobject of the Grid")]
+    public GameObject grid;
+
+    /// <summary>
+    /// speed by which the Box Grid will be rotated (-)ve value is used 
+    /// to rotate the grid in the direction of finger swipe
+    /// </summary>
+    private float rotationSpeed = -0.5f;
 
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        // To eneable the finger trail only when one finger is touching the screen
+        if (Input.GetMouseButton(0) && Input.touchCount == 1)
         {
+            // To make sure trail appears at the posision where finger start touching
             fingerTrail.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
             fingerTrail.SetActive(true);
         }
         else
         {
             fingerTrail.SetActive(false);
+        }
+
+        /**
+         * Determines if the Boxes can be destroyed or not and
+         * also used to rotate the object with 2 finger swipe action
+         **/
+        if(Input.touchCount > 1)
+        {
+            canDestroyBlocks = false;
+
+            var _touch = Input.touches[0];
+
+            if(_touch.phase == TouchPhase.Moved)
+            {
+                grid.transform.RotateAround(new Vector3(0f, 0f, 0f),Vector3.up, rotationSpeed * _touch.deltaPosition.x);
+            }
+
+        }
+        else
+        {
+            canDestroyBlocks = true;
         }
     }
 }
