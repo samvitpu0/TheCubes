64ef816be36cc592dff37e392ca85a839b794460
Samvit Purohit <samvitpu0@gmail.com>
2019-05-11 09:10:16 +0530

    Destroy Box Particles Added

--- The Cubes/TheCubes/Assets/Scripts/Gameplay/BoxDestroy.cs
+++ The Cubes/TheCubes/Assets/Scripts/Gameplay/BoxDestroy.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BoxDestroy : MonoBehaviour
 {
+    [Header("Prefab of destroy effect particle system")]
+    public GameObject boxDestroyParticles;
+
     private void OnMouseEnter()
     {
         // If two or more fingers are touching the screen then Box will not be destroyed
-        if(MainController.canDestroyBlocks)
-        Destroy(gameObject);
+        if (MainController.canDestroyBlocks)
+        {
+            MainController.numberOfBlocks--;
+            Instantiate(boxDestroyParticles, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 
-    void OnDestroy()
-    {
-        MainController.numberOfBlocks--;
-    }
+
 }
